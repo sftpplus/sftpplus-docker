6b8600f425fa9e52529ff91f72fa7e93cf7500af
Adi Roiban <adiroiban@gmail.com>
2026-07-21 13:01:39 +0100

    Try smaller images.

--- sftpplus-docker-setup.sh
+++ sftpplus-docker-setup.sh
@@ -13,15 +13,16 @@ case ${ID} in
     ubuntu|debian)
         # Get the OpenSSL and libffi libraries, the only dependencies.
         apt-get update
-        apt-get install -y openssl libffi7
+        apt-get install -y libffi7
         apt-get clean
+        rm -rf /var/lib/apt/lists/*
         ;;
     alpine)
         # Update the already-included OpenSSL libs.
         apk update
         apk upgrade
         # Get the libffi library, as this is the only dependency missing.
-        apk add libffi
+        apk add --no-cache libffi
         # Force cleaning the cache.
         rm -f /var/cache/apk/*
         ;;
